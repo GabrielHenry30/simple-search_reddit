4e80b85ac73511c398017e3c01b0a81f7c39fd03
Henry <contatohenxry@gmail.com>
2022-12-07 10:24:15 -0300

    app completed ok

--- desafio2/lib/app/features/reddit/screens/search/search_controller.dart
+++ desafio2/lib/app/features/reddit/screens/search/search_controller.dart
@@ -25,25 +25,27 @@ abstract class _SearchController with Store {
     List<String> newTitles = [];
     setError(0);
     var resp = await _redditService.getHTTP(theme);
-    if (resp?.data['error'] == 403 && resp?.data['reason'] == 'private') {
-      titles.clear();
-      setError(403);
-      changeIsLoading(false);
-      return;
-    }
-    if (resp?.statusCode == 200 && resp?.statusMessage == 'OK') {
-      responseFilter = await resp?.data['data']['children'];
-      for (int i = 0; i < responseFilter.length; i++) {
-        newTitles.add(responseFilter[i]['data']['title']);
+    try {
+      if (resp?.data['error'] == 403 && resp?.data['reason'] == 'private') {
+        titles.clear();
+        setError(403);
+        changeIsLoading(false);
+        return;
       }
-      titles = newTitles;
+      if (resp?.statusCode == 200 && resp?.statusMessage == 'OK') {
+        responseFilter = await resp?.data['data']['children'];
+        for (int i = 0; i < responseFilter.length; i++) {
+          newTitles.add(responseFilter[i]['data']['title']);
+        }
+        titles = newTitles;
+        changeIsLoading(false);
+        return;
+      }
+
+      setError(404);
       changeIsLoading(false);
       return;
-    }
-
-    setError(404);
-    changeIsLoading(false);
-    return;
+    } catch (e) {}
   }
 
   bool changeIsLoading(bool value) => isLoading = value;
